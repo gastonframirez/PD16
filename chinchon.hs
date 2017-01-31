bf6e68461d94f48351fd7f128004dad997341788
Mak Par <Mak Par>
2017-01-31 18:31:07 -0300

    Fixes #5

--- chinchon.hs
+++ chinchon.hs
@@ -57,6 +57,31 @@ nuevoMazo = [Carta n p | p <- [Treboles .. ], n <- [Uno .. ]]
 nuevoJugador :: String -> Jugador
 nuevoJugador nombre = Jugador nombre [] [] []
 
+-- Mezclar cartas
+shuffle' :: [a] -> StdGen -> ([a],StdGen)
+shuffle' xs gen = runST (do
+				g <- newSTRef gen
+				let randomRST lohi = do
+							(a,s') <- liftM (randomR lohi) (readSTRef g)
+							writeSTRef g s'
+							return a
+				ar <- newArray n xs
+				xs' <- forM [1..n] $ \i -> do
+								j <- randomRST (i,n)
+								vi <- readArray ar i
+								vj <- readArray ar j
+								writeArray ar j vi
+								return vj
+				gen' <- readSTRef g
+				return (xs',gen'))
+	where
+		n = length xs
+		newArray :: Int -> [a] -> ST s (STArray s Int a)
+		newArray n xs =  newListArray (1,n) xs
+
+shuffleIO :: [a] -> IO [a]
+shuffleIO xs = getStdRandom (shuffle' xs)
+
 -- Repartir cartas
 
 repartir :: Repartir
